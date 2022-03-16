1795b5856acdf288a51683cd278902766432feb4
asifhaiat <asifhaiat67465@gmail.com>
2022-03-16 17:34:58 +0600

    Create multiple components from an array of objects

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 
-
+const singers = [
+  {
+    name: 'agun',
+    job: 'sopno'
+  },
+  {
+    name: 'Eva Rahman',
+    job: 'Comedy'
+  },
+  {
+    name: 'Balam',
+    job: 'Ki nesha'
+  }
+]
 function App() {
+  const nakoys = ['riyaz', 'jasim', 'razzak', 'ferdous', 'manna', 'bappa raz', 'rubel'];
   return (
     <div className="App">
-      <Router name="TP Link" brand="TP Link" paragraph="Most used router all over Bangladesh"></Router>
-      <Laptop name="Mac Book" brand="Apple" paragraph="Most valuable laptop all over the world"></Laptop>
+      {
+        nakoys.map(nayok => <li>Name: {nayok}</li>)
+      }
+
+      {/* {
+        nakoys.map(nayok => <Router name= {nayok}></Router>)
+      } */}
+
+      {
+        singers.map(singer => <Router name={singer.name}></Router>)
+      }
+
+      {/* <Router name={nakoys[0]} brand="TP Link" paragraph="Most used router all over Bangladesh"></Router>
+      <Laptop name={nakoys[1]} brand="Apple" paragraph="Most valuable laptop all over the world"></Laptop>
       <Book name="Iblisher Jibon Kahini" authorName="Monowara Begum" paragraph="This Book contain Eblish Jinn History"></Book>
-      <Phone name="Apple 13 pro Max" brand="Apple" paragraph="Most valuable phone all over the world"></Phone>
+      <Phone name="Apple 13 pro Max" brand="Apple" paragraph="Most valuable phone all over the world"></Phone> */}
     </div>
   );
 }
@@ -43,7 +69,7 @@ function Book(props){
   return (
     <div className='container2'>
       <h3>{props.name}</h3>
-      <h4>{props.authorName}</h4>
+      <h4>{props.brand}</h4>
       <p>{props.paragraph}</p>
     </div>
   )
