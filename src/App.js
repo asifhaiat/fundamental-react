0895428f68344037cdf738be33b56d4755307eee
asifhaiat <asifhaiat67465@gmail.com>
2022-03-16 14:14:49 +0600

    create component and return to html

--- src/App.js
+++ src/App.js
@@ -1,60 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
 
-const singer = {
-  name: 'Hayat',
-  age: 24,
-  id: 57898,
-  favSong: 'Onek Shadhonar pore'
-};
-const innerStyle = {
-  color: 'blue',
-  backgroundColor: 'gold',
-  borderRadius: '10px',
-  border: '1px solid tomato',
-  margin: '2px',
-  padding: '10px'
-};
-const rabbatulBait = {
-  name: 'Muhtasina Jabin Arika',
-  age: 21,
-  isSheMarried: 'Yes',
-  anniversary: '31 Dec,2022',
-  favPerson: {
-    name: 'Asif Hayat',
-    age: 24,
-    mostFavPerson: 'Muhtasina Jabin Arika'
-  },
-};
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>JSX</h1>
-        <div className='container'>
-          <p>My name is Asif Hayat</p>
-        </div>
-        <div className='music'>
-          <p style={innerStyle}>Name: {singer.name} Age: {singer.age} Favorite Song: {singer.favSong} ID: {singer.id}</p>
-        </div>
-        <div className='fav-person'>
-          <p>Name: {rabbatulBait.name} Married: {rabbatulBait.isSheMarried} Anniversary: {rabbatulBait.anniversary} Favorite Person: {rabbatulBait.favPerson.name}</p>
-        </div>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router></Router>
+      <Laptop></Laptop>
+      <Book></Book>
+      <Phone></Phone>
     </div>
   );
+}
+
+function Router() {
+  return (
+    <div className='container'>
+      <h3>Name: Mi Router 4c</h3>
+      <h4>Brand: Xiaomi</h4>
+      <p>This router has four antenna and it's performance is very good.</p>
+    </div>
+  )
+}
+function Laptop(){
+  return (
+    <div className='container1'>
+      <h3>Name: HP ProBook 450 G4</h3>
+      <h4>Brand: HP</h4>
+      <p>i used in this laptop 4 year's and it's performance really satisfied to me.</p>
+    </div>
+  )
+}
+
+function Book(){
+  return (
+    <div className='container2'>
+      <h3>Name: Islamer Etihash</h3>
+      <h4>Author Name: Dr. Muhammad Ibrahim Ash-shariki</h4>
+      <p>This book contain Islamic History</p>
+    </div>
+  )
+}
+function Phone(){
+  return (
+    <div className='container3'>
+      <h3>Name: Vivo Y93</h3>
+      <h4>Brand: Vivo</h4>
+      <p>I used this phone 3year's and i really satisfied to this phone</p>
+    </div>
+  )
 }
 
 export default App;
